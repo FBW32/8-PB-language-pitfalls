59ee2aa174c26747046611806f9393e58f2062b7
LawrenzoDM <lawrenzodelmc@outlook.com>
2020-08-06 16:47:04 +0200

    add infinity and rounding

---
+++ lawlangpit.js
@@ -0,0 +1,20 @@
+// 1. NaN
+
+// NaN: not a Number
+
+let myNum = 10;
+console.log(myNum -"hello"); // NaN
+console.log(myNum *3);   // 30
+
+// 2. Rounding 
+
+console.log(Math.round(0.020000000000000004));  //0
+
+//.3 Infinity
+
+console.log(Infinity / 0);  // Infinity
+
+console.log(Infinity / Infinity);  // NaN
+
+console.log(1 / 0);   // Infinity
+
